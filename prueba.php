2182b40b97f47bc6799515e7ae89166b12bd436d
unaiurkiola <unaiurkiola@gmail.com>
2015-09-21 09:46:55 +0200

    amargos

--- prueba.php
+++ prueba.php
@@ -12,6 +12,6 @@ echo "**************";
 echo "prueba de modificacion ";
 echo "Adios boludo";
  echo "hello world ";
-
+//********************************************
 >>>>>>> 1a41d1ed5e1755fff4172bffec18c731b5b8e574
 ?>
